64af9fe8ec96f69c3ab1815ff2abcd651d639c1d
iSweetHeart <42302258+iSweetHeart@users.noreply.github.com>
2018-08-11 20:41:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,5 +31,28 @@ client.on('message', msg => {
   if (msg.content === '-invite') {
     msg.reply('https://discordapp.com/oauth2/authorize?client_id=471458890232496130&scope=bot&permissions=2146958591');
   }
-})
+});
+client.on('message', message =>{
+    let args = message.content.split(' ');
+    let prefix = '-';
+    
+    if(args[0] === `${prefix}avatar`){
+        let mentions = message.mentions.members.first()
+        if(!mentions) {
+          let sicon = message.author.avatarURL
+          let embed = new Discord.RichEmbed()
+          .setImage(message.author.avatarURL)
+          .setColor("#f7abab") 
+          .setDescription(`**${message.author.username}#${message.author.discriminator}**'s avatar :`);
+          message.channel.send({embed})
+        } else {
+          let sicon = mentions.user.avatarURL
+          let embed = new Discord.RichEmbed()
+          .setColor("#f7abab")
+          .setDescription(`**${mentions.user.username}#${mentions.user.discriminator}**'s avatar :`)
+          .setImage(sicon)
+          message.channel.send({embed})
+        }
+    };
+  });
 client.login(process.env.BOT_TOKEN);
