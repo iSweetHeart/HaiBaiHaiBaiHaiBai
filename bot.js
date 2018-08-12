fa401f7ec18d6b62085edc6ae5807fc5e9cd67e7
iSweetHeart <42302258+iSweetHeart@users.noreply.github.com>
2018-08-12 02:32:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -144,29 +144,5 @@ delete warn[message.author.id];
 
   }
 });
-const Discord = require ("discord.js");
-const client = new Discord.Client();
-   
-client.on('message', function(message) {
-    if (!message.member.hasPermissions(['ADMINISTRATOR'])){
-            let command = message.content.split(" ")[0];
-        if(message.content.includes('discord.gg')){
-        message.reply (' ')
-           if(!message.channel.guild) return message.reply('** This command only for servers**');
-     message.member.addRole(message.guild.roles.find('name', 'Muted'));
-    const embed500 = new Discord.RichEmbed()
-      .setTitle(":x: | You Got Muted!")
-            .addField(`** You Broke Our Server Rules!  **` , `**Note: if it happened by the worng way just talk to our staff members**`)
-      .addField(`by`,`Dem`)
-            .setColor("c91616")
-            .setThumbnail(`${message.author.avatarURL}`)
-            .setAuthor(message.author.username, message.author.avatarURL)
-        .setFooter(`${message.guild.name} Server`)
-     message.channel.send(embed500)
-   
-       
-    }
-    }
-});
 
 client.login(process.env.BOT_TOKEN);
