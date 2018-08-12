e9908e45e8574fc0a0e260d2357c32d036e25e48
iSweetHeart <42302258+iSweetHeart@users.noreply.github.com>
2018-08-12 02:56:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -81,7 +81,7 @@ client.on('message', async message =>{
     xp[message.author.id].level = curlvl + 1;
     let lvlup = new Discord.RichEmbed()
     .setTitle("Level Up!")
-    .setColor(purple)
+    .setColor("#6d00ad")
     .addField("New Level", curlvl + 1);
 
     message.channel.send(lvlup).then(msg => {msg.delete(5000)});
@@ -111,7 +111,7 @@ if(!xp[message.author.id]){
  
    let lvlEmbed = new Discord.RichEmbed()
    .setAuthor(message.author.username)
-   .setColor(#6d00ad)
+   .setColor("#6d00ad")
    .addField("Level", curlvl, true)
    .addField("XP", curxp, true)
    .setFooter(`${difference} XP til level up`, message.author.displayAvatarURL);
