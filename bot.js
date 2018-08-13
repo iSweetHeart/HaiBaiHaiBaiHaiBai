a36d0c350d30bce47171fa37d0728bfd9cbcd5d2
iSweetHeart <42302258+iSweetHeart@users.noreply.github.com>
2018-08-13 02:47:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -55,4 +55,24 @@ client.on('message', message =>{
         }
     };
   });
+const Discord = require("discord.js");
+const superagent = require("superagent");
+
+module.exports.run = async (bot, message, args) =>{
+
+  let{body} = await superagent
+     .get(`https://api-to.get-a.life/meme`);
+
+  let dEmbed = new Discord.RichEmbed()
+  .setColor("#ff9900")
+  .setTitle("Meme :joy:")
+  .setImage(body.url);
+
+  message.channel.send(dEmbed);
+
+}
+
+module.exports.help = {
+  name: "meme"
+}
 client.login(process.env.BOT_TOKEN);
