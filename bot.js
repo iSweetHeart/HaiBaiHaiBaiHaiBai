07b21f5df8a2e890a19bfeab62619da0299afa56
iSweetHeart <42302258+iSweetHeart@users.noreply.github.com>
2018-08-14 04:05:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -56,12 +56,12 @@ client.on('message', message =>{
     };
   });
 client.on('ready', () => {
-     client.user.setActivity("You",{type: 'WATCHING'});
+     client.user.setActivity("YOU",{type: 'WATCHING'});
 
 });
 const fs = require('fs');
 	const moment = require('moment');
-let profile = JSON.parse(fs.readFileSync("./Profile.json", "utf8"))
+let profile = JSON.parse(fs.readFileSync("/profile.json", "utf8"))
 client.on("message", message => {
   if (message.author.bot) return;
  if(!message.channel.guild)return;
